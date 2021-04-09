77ae854da91d64240a6e7d1ce6049f5b7f854061
Query <65344885+QueryOne@users.noreply.github.com>
2021-04-09 20:11:46 +1000

    Create Profiler.options.js

---
+++ core/Profiler.options.js
@@ -0,0 +1,21 @@
+/*
+  Options
+ */
+
+Profiler = (function(Profiler) {
+  let options = [
+    {},
+    {},
+  ]
+
+  let loadOptions = function() {
+    
+  }
+  
+  let saveOptions = function() {
+    
+  }
+
+
+  return Profiler
+})(typeof Profiler != 'undefined' ? Profiler : {})
